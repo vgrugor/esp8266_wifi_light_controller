195822f6e232b00d40fe1ab98deb74b69d915790
vgrugor <42821194+vgrugor@users.noreply.github.com>
2023-11-23 11:06:23 +0200

    WiFi facade refactoring - working version

--- src/Facades/WiFi/WiFiConnectionFacade.cpp
+++ src/Facades/WiFi/WiFiConnectionFacade.cpp
@@ -4,28 +4,15 @@ WiFiConnectionFacade::WiFiConnectionFacade()
 {
 }
 
-void WiFiConnectionFacade::connect(const WiFiConfig& wifiConfig)
+void WiFiConnectionFacade::initAndConnect(const WiFiConfig& wifiConfig)
 {
-    Serial.println("Set WiFi mode");
-    this->wifi.mode(WIFI_STA);
+    Serial.println("WiFi setting start");
 
-    Serial.println("Set WiFi static ip");
-    if(!this->wifi.config(wifiConfig.ip, wifiConfig.gateway, wifiConfig.subnet)) {
-        Serial.println("Error set static IP");
-    }
-
-    Serial.println("Wifi begin");
-    this->wifi.begin(wifiConfig.ssid, wifiConfig.password);
-
-    Serial.println("Connecting to WiFi...");
-    while (this->wifi.status() != WL_CONNECTED) {
-        delay(1000);
-    }
-    Serial.println("Connected to WiFi");
-
-    Serial.println("Set autoreconnect params for WiFi");
-    this->wifi.setAutoReconnect(true);
-    this->wifi.persistent(true);
+    this->setMode();
+    this->setStaticIpAddress();
+    this->begin();
+    this->connect();
+    this->setAutoreconnect();
 
     Serial.println("WiFi setting end");
 }
@@ -36,9 +23,55 @@ bool WiFiConnectionFacade::isConnected() {
 
 void WiFiConnectionFacade::reconnect() {
     Serial.println("Reconnect to WiFi");
-    while (this->wifi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.print(".");
-    }
+    
+    this->connect();
+
     Serial.println("Reconnected to WiFi");
+}
+
+void WiFiConnectionFacade::setMode() {
+    Serial.println("Set WiFi mode");
+
+    this->wifi.mode(WIFI_STA);
+
+    Serial.println("Mode saved");
+}
+
+void WiFiConnectionFacade::setStaticIpAddress() {
+    Serial.println("Set WiFi static ip");
+
+    if(!this->wifi.config(wifiConfig.ip, wifiConfig.gateway, wifiConfig.subnet)) {
+        Serial.println("Error set static IP");
+    }
+
+    Serial.println("Static IP saved");
+}
+
+void WiFiConnectionFacade::begin() {
+    Serial.println("Wifi begin");
+
+    this->wifi.begin(wifiConfig.ssid, wifiConfig.password);
+
+    Serial.println("Wifi begin end");
+
+}
+
+void WiFiConnectionFacade::connect() {
+    Serial.println("Connecting to WiFi");
+
+    while (this->wifi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    Serial.println("Connected to WiFi");
+}
+
+void WiFiConnectionFacade::setAutoreconnect() {
+    Serial.println("Set autoreconnect params for WiFi");
+
+    this->wifi.setAutoReconnect(true);
+    this->wifi.persistent(true);
+
+    Serial.println("Autoreconnect params for WiFi saved");
 }
